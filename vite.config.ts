df6f7868ba4e51b4babfdb837c037896ab2b3ef4
AngelTheAnalyst <angelawilliam262@gmail.com>
2025-02-01 15:17:23 +0300

    svg fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],      // Plugins for React support
 
   build: {
-    outDir: 'build',        // Output directory for the build
+    outDir: 'build',   
+    rollupOptions: {
+      output: {
+        format: 'es', // Ensure the output format is ES module
+      },
+    },
   },
 });
