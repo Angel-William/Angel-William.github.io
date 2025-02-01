17a16e0c22e6c7a70e21bf58fdcd5c01e276d526
AngelTheAnalyst <angelawilliam262@gmail.com>
2025-02-01 15:54:33 +0300

    almost done

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Base path for deployment
+  base: '/My-Website/', // Base path for deployment
   plugins: [react()],      // Plugins for React support
 
   build: {
